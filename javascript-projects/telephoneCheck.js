a42b959889f5db3c78fb58c203b87adc6e0ce54f
amyhenning <henning.amy@gmail.com>
2019-03-15 16:54:37 -0600

    Added more regex cases, still need to make them more flexible

--- javascript-projects/telephoneCheck.js
+++ javascript-projects/telephoneCheck.js
@@ -1,19 +1,28 @@
 function telephoneCheck(str) {
   // regex patterns for each valid format (6 of them) - but maybe consolidate them with optional characters
   // 1. 555-555-5555
-
+  let regex1 = /1?[0-9]{3}\-[0-9]{3}\-[0-9]{4}/;
   // 2. (555)555-5555
-
+  let regex2 = /1?\([0-9]{3}\)[0-9]{3}\-[0-9]{4}/;
   // 3. (555) 555-5555
-
+  let regex3 = /1?\([0-9]{3}\)\s{1}[0-9]{3}\-{0-9}/;
   // 4. 555 555 5555
-
+  let regex4 = /1?[0-9]{3}\s{1}[0-9]{3}\s{1}[0-9]{4}/;
   // 5. 5555555555
-
+  let regex5 = /1?[0-9]{10}/;
   // 6. 1 555 555 5555
+  let regex6 = /1\s{1}[0-9]{3}\s{1}[0-9]{3}\s{1}[0-9]{4}/;
 
-  let regex = /1?\w?\(?[0-9]{3}\)?\w?\-?[0-9]{3}\w?\-?[0-9]{4}/g;
-  return str.match(regex) ? true : false
+  let regexArr = [regex1, regex2, regex3, regex4, regex5, regex6];
+
+  for (let i = 0; i < regexArr.length; i++) {
+    if (!str.match(regexArr[i])) {
+      return false;
+    }
+  }
+  return true;
+  // let regex = /1?\w?\(?[0-9]{3}\)?\w?\-?[0-9]{3}\w?\-?[0-9]{4}/g;
+
   // try to match the string to each valid format
 }
 
